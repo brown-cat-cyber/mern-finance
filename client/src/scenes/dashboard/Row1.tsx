330a8282f5b0b31ff6acfeaa61a50d5f15e73e06
brown-cat-cyber <xiaohuiyuFisch@gmail.com>
2023-04-12 15:11:12 +0800

    change prefixes of importing files

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,8 +1,8 @@
 import { useGetKpisQuery } from "../../states/api"
-import DashboardBox from "../../components/DashboardBox"
+import DashboardBox from "../../components/DashboardBox.tsx"
 import { useTheme } from "@mui/material"
 import { useMemo } from "react"
-import BoxHeader from "../../components/BoxHeader"
+import BoxHeader from "../../components/BoxHeader.tsx"
 import {
   ResponsiveContainer,
   CartesianGrid,
